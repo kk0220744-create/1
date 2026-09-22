d53cc345806942beb1f7b86520a1e2c2f7d2dc1b
kk0220744-create <kk0220744@gmail.com>
2026-09-23 03:19:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -20,17 +20,17 @@ YTUB_COOKIES = """
 """
 
 # ─── BOT / DATABASE CONFIG ──────────────────────────────────────────────────────
-API_ID       = os.getenv("API_ID", "20346550")
-API_HASH     = os.getenv("API_HASH", "bc79c3bea7a626887bdc0871eecf0327")
-BOT_TOKEN    = os.getenv("BOT_TOKEN", "8936769226:AAHmUIdRQAyaX2wVqttFmQ07FSWyJqOy3Vc")
-MONGO_DB     = os.getenv("MONGO_DB", "mongodb+srv://alexaditya:alexaditya950@cluster0.7j1hfjk.mongodb.net/?appName=Cluster0")
-DB_NAME      = os.getenv("DB_NAME", "telegram_downloader")
+API_ID       = os.getenv("API_ID", "")
+API_HASH     = os.getenv("API_HASH", "")
+BOT_TOKEN    = os.getenv("BOT_TOKEN", "")
+MONGO_DB     = os.getenv("MONGO_DB", "")
+DB_NAME      = os.getenv("DB_NAME", "t_downloader")
 
 # ─── OWNER / CONTROL SETTINGS ───────────────────────────────────────────────────
-OWNER_ID     = list(map(int, os.getenv("OWNER_ID", "8460497291").split()))  # space-separated list
+OWNER_ID     = list(map(int, os.getenv("OWNER_ID", "").split()))  # space-separated list
 STRING       = os.getenv("STRING", None)  # optional session string
-LOG_GROUP    = int(os.getenv("LOG_GROUP", "-1004453123004"))
-FORCE_SUB    = int(os.getenv("FORCE_SUB", "-1004431674682"))
+LOG_GROUP    = int(os.getenv("LOG_GROUP", ""))
+FORCE_SUB    = int(os.getenv("FORCE_SUB", ""))
 
 # ─── SECURITY KEYS ──────────────────────────────────────────────────────────────
 MASTER_KEY   = os.getenv("MASTER_KEY", "gK8HzLfT9QpViJcYeB5wRa3DmN7P2xUq")  # session encryption
